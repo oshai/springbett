f41aed132bb33c8ecd27b957c8eb9428b09e0dfc
oshai <ohadshai@gmail.com>
2022-11-06 21:40:32 +0200

    initial stadium support

--- src/main/kotlin/io/github/oshai/springbett/SpringbettApplication.kt
+++ src/main/kotlin/io/github/oshai/springbett/SpringbettApplication.kt
@@ -2,28 +2,14 @@ package io.github.oshai.springbett
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
-import org.springframework.aot.hint.MemberCategory
-import org.springframework.aot.hint.RuntimeHints
-import org.springframework.aot.hint.RuntimeHintsRegistrar
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ImportRuntimeHints
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.awaitBody
 import org.springframework.web.reactive.function.server.bodyAndAwait
-import org.springframework.web.reactive.function.server.bodyToFlow
 import org.springframework.web.reactive.function.server.bodyValueAndAwait
 import org.springframework.web.reactive.function.server.coRouter
-import java.time.Instant
-import java.time.ZonedDateTime
 
 @SpringBootApplication
 class SpringbettApplication {
