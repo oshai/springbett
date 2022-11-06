0675c160b56b0ad4b5d1d98412b9a25b27ab8953
oshai <ohadshai@gmail.com>
2022-11-06 09:40:31 +0200

    initial stadium support

--- src/main/kotlin/io/github/oshai/springbett/SpringbettApplication.kt
+++ src/main/kotlin/io/github/oshai/springbett/SpringbettApplication.kt
@@ -3,9 +3,41 @@ package io.github.oshai.springbett
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.RuntimeHints
+import org.springframework.aot.hint.RuntimeHintsRegistrar
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ImportRuntimeHints
+import org.springframework.data.annotation.Id
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+import java.time.Instant
+import java.time.ZonedDateTime
+
 @SpringBootApplication
-class SpringbettApplication
+class SpringbettApplication {
+
+	@Bean
+	fun http(sr: StadiumRepository) = coRouter {
+		GET("/stadiums") {
+			ServerResponse.ok().bodyAndAwait(sr.findAll())
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringbettApplication>(*args)
 }
+
+
+interface StadiumRepository : CoroutineCrudRepository<Stadium, Int>
+
+data class Stadium(@Id val id: Int? = null, val name: String, val city: String, val capacity: Int)
--- src/test/kotlin/io/github/oshai/springbett/SpringbettApplicationTests.kt
+++ src/test/kotlin/io/github/oshai/springbett/SpringbettApplicationTests.kt
@@ -1,13 +1,21 @@
 package io.github.oshai.springbett
 
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringbettApplicationTests {
+class SpringbettApplicationTests(@Autowired val sr: StadiumRepository) {
 
 	@Test
-	fun contextLoads() {
+	fun contextLoads() = runBlocking {
+		sr.save(Stadium(name = "Al Bayt Stadium", capacity = 60000, city =  "Al Khor"))
+		val stadiums = sr.findAll()
+		Assertions.assertEquals(3, stadiums.count())
+		Assertions.assertNotNull(stadiums.last().id)
 	}
-
 }
