261320e3f7a645625fd910987b14752e442f6683
oshai <ohadshai@gmail.com>
2022-11-08 18:36:35 +0200

    non r2dbc version

--- src/test/kotlin/io/github/oshai/springbett/SpringbettApplicationTests.kt
+++ src/test/kotlin/io/github/oshai/springbett/SpringbettApplicationTests.kt
@@ -1,21 +1,13 @@
 package io.github.oshai.springbett
 
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.flow.last
-import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringbettApplicationTests(@Autowired val sr: StadiumRepository) {
+class SpringbettApplicationTests {
 
 	@Test
-	fun contextLoads() = runBlocking {
-		sr.save(Stadium(name = "Al Bayt Stadium", capacity = 60000, city =  "Al Khor", shortName = "bayt"))
-		val stadiums = sr.findAll()
-		Assertions.assertEquals(1, stadiums.count())
-		Assertions.assertNotNull(stadiums.last().id)
+	fun contextLoads() {
 	}
+
 }
