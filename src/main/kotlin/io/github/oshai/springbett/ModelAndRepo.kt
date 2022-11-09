de85e389e101f38403939fc7f50f067cda634a88
oshai <ohadshai@gmail.com>
2022-11-09 10:34:06 +0200

    initial api

--- src/main/kotlin/io/github/oshai/springbett/ModelAndRepo.kt
+++ src/main/kotlin/io/github/oshai/springbett/ModelAndRepo.kt
@@ -1,18 +1,21 @@
 package io.github.oshai.springbett
 
+import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.annotation.Id
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.data.repository.CrudRepository
 
+interface StadiumRepository : CrudRepository<Stadium, Int>
 
-interface StadiumRepository : CoroutineCrudRepository<Stadium, Int>
+@Table("stadium")
+data class Stadium(@Id var  id: Int? = null, val name: String, val shortName: String, val city: String, val capacity: Int)
 
-data class Stadium(@Id val id: Int? = null, val name: String, val shortName: String, val city: String, val capacity: Int)
+interface TeamRepository : CrudRepository<Team, Int>
 
-interface TeamRepository : CoroutineCrudRepository<Team, Int>
-
+@Table("team")
 data class Team(@Id val id: Int? = null, val name: String, val shortName: String)
 
-interface PlayerRepository : CoroutineCrudRepository<Player, Int>
+interface PlayerRepository : CrudRepository<Player, Int>
 
+@Table("player")
 data class Player(@Id val id: Int? = null, val name: String, val shortName: String)
 
--- src/main/kotlin/io/github/oshai/springbett/SpringbettApplication.kt
+++ src/main/kotlin/io/github/oshai/springbett/SpringbettApplication.kt
@@ -1,10 +1,23 @@
 package io.github.oshai.springbett
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
 
 @SpringBootApplication
-class SpringbettApplication
+class SpringbettApplication {
+
+//	@Bean
+// delete the db on startup
+//	fun cleanMigrateStrategy(): FlywayMigrationStrategy? {
+//		return FlywayMigrationStrategy { flyway ->
+//			flyway.clean()
+//			flyway.migrate()
+//		}
+//	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringbettApplication>(*args)
