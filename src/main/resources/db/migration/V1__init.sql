cd424f3fc6ea970ce05677b3755903a650933879
oshai <ohadshai@gmail.com>
2022-11-10 15:51:24 +0200

    initial security

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 
-
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create table if not exists stadium
 (
@@ -23,3 +23,25 @@ create table if not exists player
     name varchar(255) not null,
     short_name varchar(255) not null unique
 ) ;
+
+create table if not exists users
+(
+    id uuid DEFAULT uuid_generate_v4 (),
+    username varchar(255) not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email varchar(255) not null,
+    is_admin boolean not null
+) ;
+
+create table if not exists cred
+(
+    id uuid,
+    pw varchar(255) not null
+) ;
+
+insert into users(username, first_name, last_name, email, is_admin)
+values ('oshai', 'ohad', 'shai', 'dummy@gmail.com', true);
+
+insert into cred (id, pw) select id,'oshai' from users;
+
